c22b98de7586626358d2ee0c810909581a777599
Kelvin Muller <kmuller@the-s-unit.nl>
2020-12-09 20:45:40 +0100

    oneliner

--- Day9/Solution2Smallest.py
+++ Day9/Solution2Smallest.py
@@ -1,12 +1,1 @@
-from Solution import getNumbers
-
-def findNumber(number, l_numbers):
-    # for index, item in enumerate(l_numbers):
-    #     if sum(l_numbers[:(index+1)]) == number:
-    if [print(f"Answer={min(l_numbers[:(index+1)]) + max(l_numbers[:(index+1)])}") for index, item in enumerate(l_numbers) if sum(l_numbers[:(index+1)]) == number]:
-        return True
-    return False
-
-
-i = 0
-while not findNumber(257342611, getNumbers()[i:]): i+=1 
+[(print(f"Answer={min([int(x) for x in open('Day9/input.txt').read().splitlines()][i:][:(index+1)]) + max([int(x) for x in open('Day9/input.txt').read().splitlines()][i:][:(index+1)])}"), exit()) for i in range(len([int(x) for x in open('Day9/input.txt').read().splitlines()])) for index, item in enumerate([int(x) for x in open('Day9/input.txt').read().splitlines()][i:]) if sum([int(x) for x in open('Day9/input.txt').read().splitlines()][i:][:(index+1)]) == 257342611]
