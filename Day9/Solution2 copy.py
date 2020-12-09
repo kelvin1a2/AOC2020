7f2f50d37acfca78dddf956272cd3df83c0603ac
Kelvin Muller <kmuller@the-s-unit.nl>
2020-12-09 15:59:13 +0100

    funky python; sum() on list is slow!

--- Day9/Solution2 copy.py
+++ Day9/Solution2 copy.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 def getNumbers():
-    return [int(x) for x in open('Day9/input.txt').read().splitlines()]
+    # Using readlines() 
+    f = open('Day9\input.txt', 'r')
+    return [int(x.strip()) for x in f.readlines()]
 
 def findNumber(number, l_numbers):
-    count = 0 
     for index, item in enumerate(l_numbers):
-        count += item
-        if count == number:
+        if sum(l_numbers[:(index+1)]) == number:
             print(f"Contiguous range found: smalles item {min(l_numbers[:(index+1)])} largest item {max(l_numbers[:(index+1)])}. Answergi = {min(l_numbers[:(index+1)]) + max(l_numbers[:(index+1)])}")
             return True
     return False
@@ -16,6 +16,7 @@ def findNumber(number, l_numbers):
 if __name__ == "__main__":
     l_numbers = getNumbers()
     i = 0 
+    # 257342611
     while not findNumber(257342611, l_numbers[i:]):
         i+=1
     
